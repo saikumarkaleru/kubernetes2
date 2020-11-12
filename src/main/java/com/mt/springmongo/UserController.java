d1469a7022f11a941b20c924231832e058b9f852
Your Name <you@example.com>
2020-11-12 10:03:45 +0530

    f

--- src/main/java/com/mt/springmongo/UserController.java
+++ src/main/java/com/mt/springmongo/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @Controller
 @ComponentScan(basePackages = "com.mt.*")
 @RequestMapping(path = "/")
